54472d3c8ea07e2b8e53f79a39fb409b703e2dd9
cub <cub.nft@gmail.com>
2024-07-09 12:12:34 +0100

    sushi

---
+++ src/face-generator.js
@@ -0,0 +1,10 @@
+// const svgContainer = document.getElementById("svgContainer");
+// svgContainer.innerHTML = `
+//   <svg width="100" height="100">
+//     <rect class="square" width="100" height="100" style="fill:blue;stroke-width:3;stroke:rgb(0,0,0)" />
+//   </svg>
+// `;
+
+// document.querySelector(".square").addEventListener("click", function () {
+//   this.style.fill = this.style.fill === "blue" ? "red" : "blue";
+// });
