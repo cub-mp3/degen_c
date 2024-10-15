ec68479d1af4efb3521cad4ffb1c7df53031133a
cub <cub.nft@gmail.com>
2024-10-15 15:23:21 +0100

    ksi fix

---
+++ src/thick-of-it.js
@@ -0,0 +1,110 @@
+const video = document.getElementById("thickOfItVideo");
+video.playbackRate = 1;
+
+window.addEventListener("load", () => {
+  video.play();
+});
+
+const loadingScreen = document.querySelector(".loading-screen");
+
+video.addEventListener("loadeddata", () => {
+  loadingScreen.style.display = "none";
+});
+
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const source = audioContext.createMediaElementSource(video);
+
+const distortion = audioContext.createWaveShaper();
+
+function makeDistortionCurve(amount) {
+  const n_samples = 44100;
+  const curve = new Float32Array(n_samples);
+  const deg = Math.PI / 180;
+  for (let i = 0; i < n_samples; ++i) {
+    const x = (i * 2) / n_samples - 1;
+    curve[i] = ((3 + amount) * x * 20 * deg) / (Math.PI + amount * Math.abs(x));
+  }
+  return curve;
+}
+
+distortion.curve = makeDistortionCurve(0);
+distortion.oversample = "4x";
+source.connect(distortion);
+distortion.connect(audioContext.destination);
+
+let clickCount = 0;
+
+video.addEventListener("click", function () {
+  clickCount++;
+  console.log(clickCount);
+
+  const distortionAmount = clickCount * 10;
+  video.playbackRate += 0.01;
+
+  distortion.curve = makeDistortionCurve(distortionAmount);
+});
+
+function rotationForClick() {
+  let deg = 1;
+  console.log(window.innerWidth);
+
+  document.querySelectorAll(".grid-item-inner").forEach((item) => {
+    item.addEventListener("click", () => {
+      item.style.transform = `rotateY(${180 * deg}deg)`;
+      deg++;
+      if (deg === 3) {
+        deg = 1;
+      }
+    });
+  });
+}
+
+if (window.innerWidth <= 768) {
+  rotationForClick();
+}
+
+//intersection observer
+
+import screenImg from "../ksi/the-screen.avif";
+import ringImg from "../ksi/the-ring.webp";
+import penImg from "../ksi/pen.jpg";
+import kingImg from "../ksi/the-king.avif";
+
+import skeetImg from "../ksi/skeet.png";
+
+const images = {
+  screenImg: screenImg,
+  ringImg: ringImg,
+  penImg: penImg,
+  kingImg: kingImg,
+  skeetImg: skeetImg,
+};
+
+const lazyLoad = document.querySelectorAll(".grid-item-back, .skeet");
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      //   console.log(entry.target);
+      const imgElement = entry.target;
+      const backgroundImage = imgElement.getAttribute("data-bg");
+      //   console.log(backgroundImage);
+      imgSrc = `url(${images[backgroundImage]})`;
+      //   console.log(imgSrc);
+      imgElement.style.backgroundImage = imgSrc;
+      // imgElement.style.backgroundColor = `black`;
+
+      observer.unobserve(imgElement);
+    }
+  });
+});
+
+lazyLoad.forEach((image) => {
+  observer.observe(image);
+});
+
+const skeet = document.querySelector(".skeet");
+
+const skeetKey = skeet.getAttribute("data-bg");
+
+// skeet.style.backgroundImage = `url(${images[skeetKey]})`;
